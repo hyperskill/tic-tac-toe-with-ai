d227320156f3315eb516e3686c5f1e23985d461b
Gleb Kushedow <kushedow@gmail.com>
2018-10-09 18:20:25 +0300

    Dat should work

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,6 +2,9 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.print("X O X");
+        System.out.print("O X O");
+        System.out.print("X X O");        
+        
     }
 }
