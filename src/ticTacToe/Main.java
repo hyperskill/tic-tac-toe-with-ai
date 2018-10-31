ecf7fbd5f8c2950fd56453705291512772dee59f
Naydmit <Naybauerd@gmail.com>
2018-10-31 22:21:20 +0700

    Update Main.java

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,21 @@
 package ticTacToe;
 
-public class Main {
+public class TicTacToe {
+     System.out.print("Welcome Tic-Tac-Toe");
+        public static char board [][] = new char [3][3];
+    
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+       for (int i=0;i<3;i++)
+           for(int j=0;j<3;j++)
+               board[i][j]='-';
+        board[0][0]='O';
+        board[0][1]='X';
+        board[0][2]='O';
+        board[1][0]='X';
+        board[1][1]='O';
+        board[1][2]='X';
+        board[2][0]='O';
+        board[2][1]='O';
+        board[2][2]='X';
     }
 }
