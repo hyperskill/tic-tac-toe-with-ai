3e6370a76f3a53978a093a5a6a90be41f7b9a69c
Pavlo Plichko <pp@owox.com>
2018-11-03 18:14:14 +0200

    Stage #2. Creating an array with 9 elements

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,8 +2,9 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[] field = new char[] {'O', ' ', 'O', 'X', 'X', 'O', ' ', 'X', 'X'};
+        System.out.println(new char[] {field[0], ' ', field[1], ' ', field[2]});
+        System.out.println(new char[] {field[3], ' ', field[4], ' ', field[5]});
+        System.out.println(new char[] {field[6], ' ', field[7], ' ', field[8]});
     }
 }
