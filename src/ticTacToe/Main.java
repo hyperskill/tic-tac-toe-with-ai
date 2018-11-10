5a00c2994fa64cac463cf8b30a73ec03ab535c91
Pavlo Plichko <pp@owox.com>
2018-11-10 14:34:03 +0200

    Stage #7: Create a game loop

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -5,55 +5,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static void checkGameState(char[][] field) {
-        int[][][] rows = {
-                {{0, 0}, {1, 1}, {2, 2}},
-                {{0, 2}, {1, 1}, {2, 0}},
-                {{0, 0}, {0, 1}, {0, 2}},
-                {{1, 0}, {1, 1}, {1, 2}},
-                {{2, 0}, {2, 1}, {2, 2}},
-                {{0, 0}, {1, 0}, {2, 0}},
-                {{0, 1}, {1, 1}, {2, 1}},
-                {{0, 2}, {1, 2}, {2, 2}}
-        };
-        boolean gameFinished = false;
-        boolean chanceToWin = false;
-        for (int i = 0; i < 8; i++) {
-            int cx = 0;
-            int co = 0;
-            int ce = 0;
-            for (int j = 0; j < 3; j ++) {
-                int x = rows[i][j][0];
-                int y = rows[i][j][1];
-                if (field[x][y] == 'X') {
-                    cx++;
-                } else if (field[x][y] == 'O') {
-                    co++;
-                } else {
-                    ce++;
-                }
-            }
-            if (cx == 3) {
-                gameFinished = true;
-                System.out.println("X wins");
-                break;
-            } else if (co == 3) {
-                gameFinished = true;
-                System.out.println("O wins");
-                break;
-            } else if (ce + cx == 3 || ce + co == 3) {
-                chanceToWin = true;
-            }
-        }
-        if (!gameFinished) {
-            if (chanceToWin) {
-                System.out.println("Game not finished");
-            } else {
-                System.out.println("Draw");
-            }
-        }
-    }
-
     private static void printGamesState(char[][] field) {
         System.out.println(" ------- ");
         System.out.println(new char[] {'|', ' ', field[2][0], ' ', field[2][1], ' ', field[2][2], ' ','|'});
@@ -85,19 +36,6 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        char[][] field = new char[][] {
-            {'O', 'X', 'O'},
-            {' ', 'X', 'X'},
-            {' ', 'O', 'X'}
-        };
-        printGamesState(field);
-        makeEasyLevelMove(field);
-        printGamesState(field);
-//        checkGameState(field);
-//        userMove(field)
-    }
-
     private static void makeEasyLevelMove(char[][] field) {
         System.out.println("Making move level \"easy\"");
         int c = 0;
@@ -112,7 +50,74 @@ public class Main {
         }
         if (c > 0) {
             int move = new Random().nextInt(c);
-            field[freeCells[move][0]][freeCells[move][1]] = 'X';
+            field[freeCells[move][0]][freeCells[move][1]] = 'O';
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] field = new char[][] {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+        int[][][] rows = {
+                {{0, 0}, {1, 1}, {2, 2}},
+                {{0, 2}, {1, 1}, {2, 0}},
+                {{0, 0}, {0, 1}, {0, 2}},
+                {{1, 0}, {1, 1}, {1, 2}},
+                {{2, 0}, {2, 1}, {2, 2}},
+                {{0, 0}, {1, 0}, {2, 0}},
+                {{0, 1}, {1, 1}, {2, 1}},
+                {{0, 2}, {1, 2}, {2, 2}}
+        };
+
+        printGamesState(field);
+
+        boolean gameFinished = false;
+        boolean isAIMove = false;
+
+        while (!gameFinished) {
+            if (isAIMove) {
+                makeEasyLevelMove(field);
+            } else {
+                userMove(field);
+            }
+            printGamesState(field);
+
+            isAIMove = !isAIMove;
+
+            boolean chanceToWin = false;
+            for (int i = 0; i < 8; i++) {
+                int cx = 0;
+                int co = 0;
+                int ce = 0;
+                for (int j = 0; j < 3; j++) {
+                    int x = rows[i][j][0];
+                    int y = rows[i][j][1];
+                    if (field[x][y] == 'X') {
+                        cx++;
+                    } else if (field[x][y] == 'O') {
+                        co++;
+                    } else {
+                        ce++;
+                    }
+                }
+                if (cx == 3) {
+                    gameFinished = true;
+                    System.out.println("X wins");
+                    break;
+                } else if (co == 3) {
+                    gameFinished = true;
+                    System.out.println("O wins");
+                    break;
+                } else if (ce + cx == 3 || ce + co == 3) {
+                    chanceToWin = true;
+                }
+            }
+            if (!gameFinished && !chanceToWin) {
+                System.out.println("Draw");
+                gameFinished = true;
+            }
         }
     }
 }
