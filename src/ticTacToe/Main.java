eb9e840529dae9b4506798f1fa9d8cb2ad4ca28c
JoraTracktor <39220306+JoraTracktor@users.noreply.github.com>
2018-11-11 19:31:55 +0300

    Update Main.java

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,8 +2,11 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X X");
-        System.out.println("X O O");
+       String[] tic = {"O"," ","O","X","X","O"," ","X","X"}
+        
+        
+       System.out.println(tic[0],+" "+tic[1],+" "+tic[2]);
+       System.out.println(tic[3],+" "+tic[4],+" "+tic[5]);
+       System.out.println(tic[6],+" "+tic[7],+" "+tic[8]);
     
 }
