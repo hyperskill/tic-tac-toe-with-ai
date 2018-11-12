a6f59b91664fd6b71e92bd6622c9a8aab65ab89b
JoraTracktor <39220306+JoraTracktor@users.noreply.github.com>
2018-11-12 10:00:12 +0300

    Update Main.java

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,12 +1,100 @@
 package ticTacToe;
 
 public class Main {
-    public static void main(String[] args) {
-       String[] tic = {"O"," ","O","X","X","O"," ","X","X"}
-        
-        
-       System.out.println(tic[0],+" "+tic[1],+" "+tic[2]);
-       System.out.println(tic[3],+" "+tic[4],+" "+tic[5]);
-       System.out.println(tic[6],+" "+tic[7],+" "+tic[8]);
     
+    public static String[][] arr = new String [3][3];
+    
+    public static void main(String[] args) {
+    Scanner in = new Scanner (System.in);
+	    int random_number;
+	    int label;
+	       
+	    for (int i = 0; i < 3; i++) 
+	    	for (int j = 0; j < 3; j++) {
+				if ((random_number = (int) (Math.random() * 3)) == 0)
+					arr[i][j] = "X";
+				else if (random_number == 1)
+					arr[i][j] = "O";
+				else if (random_number == 2)
+					arr[i][j] = "X";
+	    	}
+	    
+	    for (int i = 0; i < 3; i++) {
+	    	for (int j = 0; j < 3; j++) {
+	    		System.out.print(arr[i][j] + " ");
+	    	}	
+	    	System.out.println();
+	    }
+	    
+	    switch (label = CheckWinner() ) {
+	    case 1:
+	    	System.out.println("X wins");
+	    	break;
+	    case 2:
+	    	System.out.println("O wins");
+	    	break;
+	    case 3:
+	    	System.out.println("Game not finished");
+	    	break;
+	    case 0:
+	    	System.out.println("Draw");
+	    	break;
+	    }
+	    
+	    
+		
+		
+	}
+	
+	public static int CheckWinner() {
+		String line = null;
+		for (int i = 0; i < 8; i++) {
+			switch(i) {
+				case 0:
+					line = arr[0][0] + arr[0][1] + arr[0][2];
+					break;
+				case 1:
+					line = arr[1][0] + arr[1][1] + arr[1][2];
+					break;
+				case 2:
+					line = arr[2][0] + arr[2][1] + arr[2][2];
+					break;
+				case 3:
+					line = arr[0][0] + arr[1][0] + arr[2][0];
+					break;
+				case 4:
+					line = arr[0][1] + arr[1][1] + arr[2][1];
+					break;
+				case 5:
+					line = arr[0][2] + arr[1][1] + arr[2][2];
+					break;
+				case 6:
+					line = arr[0][0] + arr[1][1] + arr[2][2];
+					break;
+				case 7:
+					line = arr[2][0] + arr[1][1] + arr[0][2];
+					break;
+			}
+			
+			if (line.equals("XXX"))
+				return 1;
+			else if (line.equals("OOO"))
+				return 2;
+			else if (CheckWhitespace() == true) 
+				return 3;
+			
+			}
+		return 0;
+	}
+	
+	
+	public static boolean CheckWhitespace() {
+		boolean label = false;  
+	    for (int i = 0; i < 3; i++) 
+	    	for (int j = 0; j < 3; j++) 
+	    		if (arr[i][j] == " ")
+	    			label = true;
+	    return label;   
+    
+}
 }
