65c4be4133fc0d71baf17ab5009e179031639639
AnnaSoledat <nana10-56@mail.ru>
2018-11-14 03:59:56 +0300

    print random state using array

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,9 +1,37 @@
 package ticTacToe;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("X O X \n" +
+        /*System.out.print("X O X \n" +
                 "O X O \n" +
-                "X X O ");
+                "X X O ");*/
+        char[] arr = new char[9];
+        final Random random = new Random();
+        for (int i = 0; i<9; i++) {
+            int a = random.nextInt(3);
+            switch (a) {
+                case 0:
+                    arr[i] = ' ';
+                    break;
+                case 1:
+                    arr[i] = 'X';
+                    break;
+                case 2:
+                    arr[i] = 'O';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        for (int i = 0; i<9; i++) {
+            if (((i+1)%3) == 0) {
+                System.out.print(arr[i]+"\n");
+            } else {
+                System.out.print(arr[i]+" ");
+            }
+        }
     }
 }
