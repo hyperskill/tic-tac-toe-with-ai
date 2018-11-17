38a6f4f7db1940843f0aec9cd2bd7e91571673de
dupliaka <dupliaka@gmail.com>
2018-11-17 14:36:53 +0300

    Print the demo view of ttt

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,20 @@
 package ticTacToe;
 
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+        String[][] s = new String[][]{
+                  new String[]{"X", "O", "X"}
+                , new String[]{"O", "X", "O"}
+                , new String[]{"X", "X", "O"}};
+
+        Arrays.stream(s).forEach(x-> {Arrays.stream(x).forEach(System.out::print);
+            System.out.println();
+        });
     }
+
+
 }
