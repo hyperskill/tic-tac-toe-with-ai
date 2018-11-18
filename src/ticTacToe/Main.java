180ec34fde3deeab2f5e2d22a93141572700aee5
Ilya <ilia.bogatyrve>
2018-11-18 21:18:40 +0300

    show random field completed

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,6 +2,14 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+        		if (Math.random()>=0.5) {
+        			System.out.print("X ");
+        		}
+        		else {System.out.print("O ");}
+        	}
+        	System.out.println();
+        }
     }
 }
