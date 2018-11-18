7265a6dbe0df289a4533b054827284508cbef14f
AIexMoran <homin.sashawork@gmail.com>
2018-11-18 21:20:47 +0300

    "Simple gamefield"

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,8 +2,12 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[] elements = {'X', ' ', 'O', 'X', 'O', ' ', 'O', ' ', 'X'};
+        for (int i = 0; i < 9; i++) {
+            System.out.print(elements[i] + " ");
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            }
+        }
     }
 }
