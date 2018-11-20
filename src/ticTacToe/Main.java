5ffc078f1967aa3567107214d19cae29183ac079
Ilya <ilia.bogatyrve>
2018-11-20 10:43:05 +0300

    Stage 2 completed

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,15 +1,31 @@
 package ticTacToe;
 
+import java.util.Arrays;
+
 public class Main {
+	static char[] array = new char[9];
+	static void displayField(char[] array) {
+		int k = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(array[k++]);
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
     public static void main(String[] args) {
-        for (int i=0; i<3; i++) {
-        	for (int j=0; j<3; j++) {
-        		if (Math.random()>=0.5) {
-        			System.out.print("X ");
-        		}
-        		else {System.out.print("O ");}
-        	}
-        	System.out.println();
+    	for (int i=0; i<9; i++) {
+        	if (Math.random()>=0.66) {
+        		array[i] = 'X';
+       		}
+       		else if (Math.random() <= 0.33) {
+       			array[i] = 'O';
+       			}
+       		else {
+       			array[i] = ' ';
+       		}
         }
+    	displayField(array);
     }
 }
