f470f06a7606d7d6cc3095b9049c5ea0ad02e82f
SkaviCos <skavinski_90@mail.ru>
2018-11-23 16:24:13 +0300

    Update FirstStage.java
    Second solve

--- src/ticTacToe/FirstStage.java
+++ src/ticTacToe/FirstStage.java
@@ -1,8 +1,12 @@
 public class FirstStage {
   public static void main(String[] args) {
-    System.out.println("0"+" "+"X"+" "+"X");
-    System.out.println("X"+" "+"0"+" "+"X");
-    System.out.println("X"+" "+"0"+" "+"0");
+    String tic[] = {"0","X"," ","X","0","0","X","0"," "};
+    System.out.println(tic[0]+" " +tic[1]+" "+tic[2] +
+              "\n" + tic[3] + " " + tic[4] + " " + tic[5] +
+              "\n" + tic[6] + " " + tic[7] + " " + tic[8]);
+    //System.out.println("0"+" "+"X"+" "+"X");
+    //System.out.println("X"+" "+"0"+" "+"X");
+    //System.out.println("X"+" "+"0"+" "+"0");
 
   }
 }
