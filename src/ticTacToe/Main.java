e03c0a18cd4a50b462a152f44fa0298c16269a68
Ilya <ilia.bogatyrve>
2018-11-28 21:57:59 +0300

    Stage3 complete

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,31 +1,102 @@
 package ticTacToe;
 
-import java.util.Arrays;
-
 public class Main {
-	static char[] array = new char[9];
-	static void displayField(char[] array) {
-		int k = 0;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				System.out.print(array[k++]);
+	static int dim = 3;
+	static char[][] array = new char[dim][dim];
+	//SHOW CURRENT FIELD
+	static void displayField(char[][] array) {
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				System.out.print(array[i][j]);
 				System.out.print(' ');
 			}
 			System.out.println();
 		}
 	}
+	static String displayStage(char[][] array) {
+		char current;
+		boolean flag = false;
+		for (int i = 0; i < dim; i++) {
+			current = 'z';
+			if (array[i][0] != ' ') {current = array[i][0];}
+			else {continue;}
+			for (int j = 0; j < dim; j++) {
+				if (array[i][j] == current) {
+					current = array[i][j];
+					flag = true;
+				}
+				else {flag = false;break;}
+				}
+			if (flag) {return current+" wins";}
+			}
+		for (int i = 0; i < dim; i++) {
+			current = 'z';
+			if (array[0][i] != ' ') {current = array[0][i];}
+			else {continue;}
+			for (int j = 0; j < dim; j++) {
+				if (array[j][i] == current) {
+					current = array[j][i];
+					flag = true;
+				}
+				else {flag = false;break;}
+				}
+			if (flag) {return current+" wins";}
+			}
+		
+		current = 'z';
+		if (array[0][0] != ' ') {
+			current = array[0][0];
+			for (int i = 0; i < dim; i++) {
+				if (array[i][i] == current) {
+					current = array[i][i];
+					flag = true;
+					}
+				else {flag = false;break;}
+			}
+			if (flag) {return current+" wins";}
+		}
+			
+		current = 'z';
+		if (array[0][dim-1] != ' ') {
+			current = array[0][dim-1];
+			for (int i = 0; i < dim; i++) {
+				if (array[i][dim-1-i] == current) {
+					current = array[i][dim-1-i];
+					flag = true;
+					}
+				else {flag = false;break;}
+				}
+			if (flag) {return current+" wins";}
+
+		}
+		
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				if (array[i][j] == ' ') {return "Game not finished";}
+			}
+		}
+		return "Draw";
+	}
+	//GENERATE RANDOM FIELD
+	static void generateField(char[][] array) {
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				if (Math.random()>=0.66) {
+	        		array[i][j] = 'X';
+	       		}
+	       		else if (Math.random() <= 0.33) {
+	       			array[i][j] = 'O';
+	       			}
+	       		else {
+	       			array[i][j] = ' ';
+	       		}
+			}
+		}
+	}
+	
     public static void main(String[] args) {
-    	for (int i=0; i<9; i++) {
-        	if (Math.random()>=0.66) {
-        		array[i] = 'X';
-       		}
-       		else if (Math.random() <= 0.33) {
-       			array[i] = 'O';
-       			}
-       		else {
-       			array[i] = ' ';
-       		}
-        }
+    	generateField(array);
     	displayField(array);
+    	System.out.println(displayStage(array));
     }
 }
