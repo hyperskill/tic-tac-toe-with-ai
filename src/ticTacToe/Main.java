a75b866a604badd56d04ce7666813aac5449624c
sergeipetriankin <sergei_petriankin@epam.com>
2018-11-28 15:52:04 +0300

    Simple game filed is printed

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,10 +1,34 @@
 package ticTacToe;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        String[][] values = new String[3][3];
+
         for (int i = 0; i < 3; i++) {
-            String value = i % 2 == 0 ? "X" : "O";
-            System.out.printf("%s %s %s\n", value, value, value);
+            for (int j = 0; j < 3; j++) {
+                values[i][j] = fillValue();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%s ", values[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static String fillValue() {
+        int randomInt = new Random().nextInt(10);
+        if (randomInt % 3 == 0) {
+            return "X";
+        } else if (randomInt % 2 == 0) {
+            return "O";
+        } else {
+            return " ";
         }
     }
 }
