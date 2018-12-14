b5b256e510f2b0f8f0d80c99cc51b3afd6e57424
omnioracle <cryptocovenant@gmail.com>
2018-12-15 00:52:41 +0100

    First implementation

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,11 @@
 package ticTacToe;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("\tX \tO \tX");
+        System.out.println("\tO \tX \tO");
+        System.out.println("\tX \tX \tO");
+
     }
 }
