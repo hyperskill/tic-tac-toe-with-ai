348f58598c9d369e3e2a4fda8e44c3d14ea43e40
Nikita Karpov <bonilka@gmail.com>
2018-12-23 14:32:29 +0300

    print static game state

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,36 @@
 package ticTacToe;
 
 public class Main {
+    private static char SYMBOL_0 = 'X';
+    private static char SYMBOL_1 = 'O';
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        printMatrix();
+    }
+    private static void printMatrix(){
+        System.out.print(Main.SYMBOL_0);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_1);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_0);
+        System.out.print("\t");
+
+        System.out.println();
+
+        System.out.print(Main.SYMBOL_0);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_0);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_1);
+        System.out.print("\t");
+
+        System.out.println();
+
+        System.out.print(Main.SYMBOL_1);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_1);
+        System.out.print("\t");
+        System.out.print(Main.SYMBOL_0);
+        System.out.print("\t");
+
     }
 }
