c26822ac19bea57f5babebdf0f4c6cab0c45bee9
Nikita Karpov <bonilka@gmail.com>
2018-12-23 14:50:36 +0300

    add fields

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,36 +1,39 @@
 package ticTacToe;
 
 public class Main {
-    private static char SYMBOL_0 = 'X';
-    private static char SYMBOL_1 = 'O';
+    private static char X = 'X';
+    private static char O = 'O';
+    private static char EMPTY = ' ';
+    private final static int ROW = 3;
+    private final static int COL = ROW;
+    private final static int SIZE = ROW * COL;
+
     public static void main(String[] args) {
-        printMatrix();
+        char[] matrix = new char[Main.SIZE];
+        randomFill(matrix);
+        printMatrix(matrix);
     }
-    private static void printMatrix(){
-        System.out.print(Main.SYMBOL_0);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_1);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_0);
-        System.out.print("\t");
+    private static void printMatrix(char[] matrix){
+        for (int i = 0; i < matrix.length; i++){
+            System.out.print(matrix[i]);
+                if(i > 0 && (i + 1) % Main.COL == 0){
+                    System.out.println();
+                } else{
+                    System.out.print("\t");
+                }
+        }
+    }
+    private static void randomFill(char[] matrix){
+        matrix[0] = Main.EMPTY;
+        matrix[1] = Main.X;
+        matrix[2] = Main.O;
 
-        System.out.println();
+        matrix[3] = Main.O;
+        matrix[4] = Main.X;
+        matrix[5] = Main.EMPTY;
 
-        System.out.print(Main.SYMBOL_0);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_0);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_1);
-        System.out.print("\t");
-
-        System.out.println();
-
-        System.out.print(Main.SYMBOL_1);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_1);
-        System.out.print("\t");
-        System.out.print(Main.SYMBOL_0);
-        System.out.print("\t");
-
+        matrix[6] = Main.EMPTY;
+        matrix[7] = Main.X;
+        matrix[8] = Main.X;
     }
 }
