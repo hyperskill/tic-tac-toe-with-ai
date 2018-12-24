77a6504b41d741753229b25d4433f56b097ebce4
cyprusScorpion <vladilen.eleonskiy@gmail.com>
2018-12-24 21:47:03 +0300

    commit task_2

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -8,11 +8,13 @@ public class Main {
     Random random = new Random();
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix.length; j++) {
-        int y = random.nextInt(2);
+        int y = random.nextInt(3);
         if (y == 0) {
         matrix[j][i] = 'O';
-        } else {
+        } else if (y == 1) {
           matrix[j][i] = 'X';
+        } else {
+          matrix[j][i] = ' ';
         }
       }
     }
