bc1cbd1071ec84d74c480bed7f41fca15cdcd799
cyprusScorpion <vladilen.eleonskiy@gmail.com>
2018-12-26 03:02:32 +0300

    commit task_3 (ticTacToe)

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,28 +1,9 @@
 package ticTacToe;
 
-import java.util.Random;
-
 public class Main {
   public static void main(String[] args) {
-    char[][] matrix = new char[3][3];
-    Random random = new Random();
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix.length; j++) {
-        int y = random.nextInt(3);
-        if (y == 0) {
-        matrix[j][i] = 'O';
-        } else if (y == 1) {
-          matrix[j][i] = 'X';
-        } else {
-          matrix[j][i] = ' ';
-        }
-      }
-    }
-    for (char[] ch : matrix) {
-      for (char c : ch) {
-        System.out.print(c + " ");
-      }
-      System.out.println();
-    }
+    Game game = new Game();
+    game.start();
+    game.over();
   }
 }
