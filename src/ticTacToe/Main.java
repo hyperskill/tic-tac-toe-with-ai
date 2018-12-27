c89e0551612b7d6eacc5138c4b95e8f62698e052
Hexronimo <leafcat@mail.ru>
2018-12-27 22:15:40 +0300

    Generates random 3x3 field

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,6 +2,24 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Field(3);
+    }
+
+    static void Field(int width) {
+        char[][] field = new char[width][width];
+        for (int i=0; i<width; i++) {
+            for (int j = 0; j < width; j++) {
+                field[i][j] = GenerateXorO();
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static char GenerateXorO () {
+        char xoro;
+        xoro = 'O';
+        if ((int)(Math.random() * 2) + 0 == 1) xoro = 'X';
+        return xoro;
     }
 }
