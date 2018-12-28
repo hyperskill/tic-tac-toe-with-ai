2cc0084a8c2ec295f48fe88fcb4953430ddc39a6
Hexronimo <leafcat@mail.ru>
2018-12-28 10:31:27 +0300

    With winner checking (step 3)

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,7 +2,8 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        Field(3);
+        int width = 3;
+        Field(width);
     }
 
     static void Field(int width) {
@@ -14,6 +15,7 @@ public class Main {
             }
             System.out.println();
         }
+        IfWinner(field, width);
     }
 
     static char GenerateXorO () {
@@ -21,6 +23,99 @@ public class Main {
         xoro = 'O';
         if ((int)(Math.random() * 3) + 0 == 1) xoro = 'X';
         if ((int)(Math.random() * 3) + 0 == 2) xoro = ' ';
+
         return xoro;
     }
+
+    static boolean IfWinner(char[][] field, int width) {
+        int count_x = 0;
+        int count_o = 0;
+        boolean posibilities = false;
+
+        /*
+        Checking first diagonal
+        X__
+        _X_
+        __X
+         */
+        for (int i=0; i<width; i++) {
+            if (field[i][i]=='X') count_x++;
+            if (field[i][i]=='O') count_o++;
+        }
+        if (!(count_x>0 && count_o>0)) posibilities = true;
+        if (ChekingWinner(count_x,count_o,width)) return false;
+
+            count_x = 0;
+            count_o = 0;
+
+            ;
+        /*
+        Checking second diagonal
+        __X
+        _X_
+        X__
+        */
+            for (int i = width - 1; i >= 0; i--) {
+                    if (field[i][width-1-i] == 'X') count_x++;
+                    if (field[i][width-1-i] == 'O') count_o++;
+            }
+        if (!(count_x>0 && count_o>0)) posibilities = true;
+        if (ChekingWinner(count_x,count_o,width)) return false;
+
+            count_x = 0;
+            count_o = 0;
+
+        /*
+        Checking columns
+        X__
+        X__
+        X__
+        */
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                if (field[i][j]=='X') count_x++;
+                if (field[i][j]=='O') count_o++;
+                }
+            if (!(count_x>0 && count_o>0)) posibilities = true;
+            if (ChekingWinner(count_x,count_o,width)) return false;
+            count_x = 0;
+            count_o = 0;
+
+            }
+
+
+
+
+         /*
+        Checking lines
+        ___
+        XXX
+        ___
+         */
+
+            for (int j = 0; j < width; j++) {
+                for (int i = 0; i < width; i++) {
+                    if (field[i][j]=='X') count_x++;
+                    if (field[i][j]=='O') count_o++;
+                    }
+                if (!(count_x>0 && count_o>0)) posibilities = true;
+                if (ChekingWinner(count_x,count_o,width)) return false;
+                count_x = 0;
+                count_o = 0;
+
+                }
+
+        if (posibilities == true) {System.out.println("Game not over yet");return false;}
+
+        System.out.println("Draw");
+            return true;
+    }
+
+    static boolean ChekingWinner(int count_x, int count_o, int width){
+        if (count_x==width) {System.out.println("X wins"); return true;}
+        if (count_o==width) {System.out.println("O wins"); return true;}
+        return false;
+    }
+
 }
