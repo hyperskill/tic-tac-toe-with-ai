1a7d4751a949574470c890550b6cc55b63b35cb5
Hexronimo <leafcat@mail.ru>
2018-12-31 01:58:41 +0300

    Stage #7: Create a game loop

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -10,21 +10,23 @@ public class Main {
         Player human = new Player('X');
         Player comp = new Player('O');
         String mode = "easy";
-
+        field.DrawField();
+        System.out.println();
         while (continue_game) {
-            do {
-                comp.RandomValue(field.width, mode);
-            } while (field.SetValueComp(comp.GetFirst(), comp.GetSecond(), comp.pic, mode) == false);
-            field.DrawField();
-            continue_game = field.IfWinner(human.pic, comp.pic);
-            System.out.println();
-            if (continue_game == false) break;
+
             do {
                 human.InputValue(field.width);
             } while (field.SetValueHuman(human.GetFirst(), human.GetSecond(), human.pic) == false);
             field.DrawField();
             continue_game = field.IfWinner(human.pic, comp.pic);
-            System.out.println();
+
+            if (continue_game == false) break;
+            do {
+                comp.RandomValue(field.width, mode);
+            } while (field.SetValueComp(comp.GetFirst(), comp.GetSecond(), comp.pic, mode) == false);
+            field.DrawField();
+            continue_game = field.IfWinner(human.pic, comp.pic);
+
         }
 
 
