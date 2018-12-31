21007ccbff29b99afa6b55ca9b01a974409ec1b2
Orest Shemelyuk <orestshemelyuk@gmail.com>
2018-12-31 16:27:26 +0200

    Draw state game

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,10 @@
 package ticTacToe;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.print(" X O X\n" +
+                         " O X O\n" +
+                         " X X O");
     }
 }
