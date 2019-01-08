b8a1184519b34e635ff0b2e2d6057b4f4bf04eed
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-01-09 01:27:22 +0200

    create a game with easy level only

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,46 +1,58 @@
 package ticTacToe;
-import java.io.Reader;
+
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         int n = 3;
         char [][]state = new char[][] {
-                {' ', 'O', 'X'},
-                {' ', 'O', ' '},
-                {' ', ' ', 'X'}
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
         };
 
-        //inputHuman(state, n);
-        generateEasy(state, n);
-    }
-
-    static void drawState(char[][] state, int size){
-        System.out.println("---------");
-        for (int i = 0; i < size; i++){
-            System.out.print("| ");
-            for (int j = 0; j < size; j++){
-                System.out.print(state[i][j] + " ");
+        System.out.println("Choose level of game: \n" +
+                "1.Easy\n" +
+                "2.Medium\n" +
+                "3.Hard");
+        int choose;
+        do {
+            System.out.print("Your select: ");
+            while (!scan.hasNextInt()) {
+                String input = scan.next();
+                System.out.println("You should enter numbers!");
             }
-            System.out.println("|");
+            choose =  scan.nextInt();
+        }while (choose < 1 || choose > 3);
+
+        if (choose == 1){
+            easyGame(state, n);
         }
-        System.out.println("---------");
+        else if (choose == 2){
+            //medium
+        }
+        else {
+            //hard
+        }
+
     }
 
-    static boolean checkSell(char[][] state, int indexI, int indexJ){
-        if(indexI < 0 || indexI >= 3 || indexJ < 0 || indexJ >= 3){
-            System.out.println("Coordinates should be from 1 to 3!");
-            return false;
-        }
-        if (state[indexI][indexJ] != ' '){
-            System.out.println("This cell is occupied! Choose another one!");
-            return false;
-        }
-        return true;
+    private static void easyGame(char[][]state, int n){
+        boolean flag;
+        do {
+            inputHuman(state, n);
+            flag = checkGame(state,n);
+            if (!flag) {
+                generateEasy(state, n);
+                flag = checkGame(state,n);
+
+            }
+
+        }while (!flag);
     }
 
-    static void inputHuman(char[][]state, int n){
+    private static void inputHuman(char[][]state, int n){
         Scanner scan = new Scanner(System.in);
         int indexI;
         int indexJ;
@@ -62,13 +74,12 @@ public class Main {
                 break;
             }
 
-            
+
         }while (true);
         state[indexI][indexJ] = 'X';
-        drawState(state,n);
     }
 
-    static void generateEasy(char[][]state, int n){
+    private static void generateEasy(char[][]state, int n){
         drawState(state, n);
         System.out.println("Making move level \"easy\"");
         Random random = new Random();
@@ -77,10 +88,108 @@ public class Main {
             int randJ = random.nextInt(3);
 
             if(state[randI][randJ] == ' '){
-                state[randI][randJ] = 'X';
+                state[randI][randJ] = 'O';
                 break;
             }
         }while (true);
-        drawState(state, n);
     }
+
+    private static boolean checkGame(char[][]state, int n) {
+        boolean flag = false;
+        int stepRow;
+        int stepCol;
+        for (int i = 0; i < n; i++) {
+            stepRow = 1;
+            stepCol = 1;
+            for (int j = 0; j < n - 1; j++) { //check for rows
+                if (state[i][j] == state[i][j + 1]) {
+                    stepRow++;
+                } else {
+                    break;
+                }
+                if (stepRow == 3) {
+                    if (state[i][j] == 'X') {
+                        drawState(state,n);
+                        System.out.println("X wins");
+                        flag = true;
+                    }
+                    if (state[i][j] == 'O') {
+                        drawState(state,n);
+                        System.out.println("O wins");
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) {
+                break;
+            }
+            for (int j = 0; j < n - 1; j++) {//check for col
+                if (state[j][i] == state[j + 1][i]) {
+                    stepCol++;
+                } else {
+                    break;
+                }
+                if (stepCol == 3) {
+
+                    if (state[j][i] == 'X') {
+                        drawState(state,n);
+                        System.out.println("X wins");
+                        flag = true;
+                    }
+                    if (state[j][i] == 'O') {
+                        drawState(state,n);
+                        System.out.println("O wins");
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) {
+                break;
+            }
+            if (state[0][0] == state[1][1] && state[0][0] == state[2][2] ||
+                    state[0][2] == state[1][1] && state[1][1] == state[2][0]) {
+
+                if (state[1][1] == 'X') {
+                    drawState(state,n);
+                    System.out.println("X wins");
+                    flag = true;
+                }
+                if (state[1][1] == 'O') {
+                    drawState(state,n);
+                    System.out.println("O wins");
+                    flag = true;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+
+        return flag;
+    }
+
+    private static boolean checkSell(char[][] state, int indexI, int indexJ){
+        if(indexI < 0 || indexI >= 3 || indexJ < 0 || indexJ >= 3){
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+        if (state[indexI][indexJ] != ' '){
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+        return true;
+    }
+
+    private static void drawState(char[][] state, int size){
+        System.out.println("---------");
+        for (int i = 0; i < size; i++){
+            System.out.print("| ");
+            for (int j = 0; j < size; j++){
+                System.out.print(state[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
 }
