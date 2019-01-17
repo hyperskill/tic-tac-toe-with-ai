acd8115f3000148cdc1564b0949e2997be686d75
Sensdoo <sensibleprogrammer@gmail.com>
2019-01-17 18:39:16 +0300

    Stage #2: Create a simple game field

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,7 +1,29 @@
 package ticTacToe;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("X O X \n" + "O X O \n" + "X X O ");
+        Random random = new Random();
+        char[][] fieldValues = new char[3][3];
+        String values = "XO ";
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int index = random.nextInt(3);
+                fieldValues[i][j] = values.charAt(index);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j < 2) {
+                    System.out.print(fieldValues[i][j] + " ");
+                } else {
+                    System.out.print(fieldValues[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
