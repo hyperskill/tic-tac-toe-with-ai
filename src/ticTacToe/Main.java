da4ca571afd2b65781e227637841b042103d484d
MrMurder <karp.jaroslav@gmail.com>
2019-01-17 23:39:09 +0200

    Print a state of the game field

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,8 +2,12 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[][] ch = new char[][]{{'O', ' ', 'O',}, {'X', 'X', 'O'}, {' ', 'X', 'X'}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(ch[i][j]);
+            }
+            System.out.print('\n');
+        }
     }
 }
