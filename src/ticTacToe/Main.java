55a21b4d30537cc5c5a9d43f4d3bc1754cf53c7e
Sensdoo <sensibleprogrammer@gmail.com>
2019-01-18 13:05:16 +0300

    Stage #4: Create a human move

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,13 +2,29 @@ package ticTacToe;
 
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean gameOver = false;
+        String result;
         char[][] fieldValues = init();
-        draw(fieldValues);
-        System.out.println(checkState(fieldValues));
+        Scanner scanner = new Scanner(System.in);
 
+        while (!gameOver) {
+            draw(fieldValues);
+            result = checkState(fieldValues);
+            if ("Game not finished.".equals(result)) {
+                System.out.print("Enter the coordinates: ");
+                int x = scanner.nextInt() - 1;
+                int y = scanner.nextInt() - 1;
+                fieldValues = humanMove(x, y, fieldValues);
+            } else {
+                System.out.println();
+                System.out.println(result);
+                gameOver = true;
+            }
+        }
     }
 
     private static char[][] init() {
@@ -25,15 +41,24 @@ public class Main {
         return  fieldValues;
     }
 
+    private static char[][] humanMove(int x, int y, char[][] fieldValues) {
+        if (fieldValues[x][y] == ' ') {
+            fieldValues[x][y] = 'X';
+        } else {
+            System.out.println("Incorrect input. Try again.");
+        }
+        return fieldValues;
+    }
+
     private static String checkState(char[][] fieldValues) {
 
-        int col = 0;
-        int row = 0;
 
         for (int i = 0; i < 3; i++) {
+            int col = 0;
+            int row = 0;
 
             //row check{
-            if (fieldValues[i][0] == fieldValues[i][1] && fieldValues[i][0] == fieldValues[i][2]) {
+            if (fieldValues[i][0] == fieldValues[i][1] && fieldValues[i][0] == fieldValues[i][2] && fieldValues[i][0] != ' ') {
                 row++;
             }
 
@@ -46,7 +71,7 @@ public class Main {
             }
 
             //col check
-            if (fieldValues[0][i] == fieldValues[1][i] && fieldValues[0][i] == fieldValues[2][i]) {
+            if (fieldValues[0][i] == fieldValues[1][i] && fieldValues[0][i] == fieldValues[2][i] && fieldValues[0][i] != ' ') {
                 col++;
             }
 
@@ -60,14 +85,14 @@ public class Main {
         }
 
         //diagonal check
-        if (fieldValues[0][0] == fieldValues[1][1] && fieldValues[0][0] == fieldValues[2][2]) {
+        if (fieldValues[0][0] == fieldValues[1][1] && fieldValues[1][1] == fieldValues[2][2] && fieldValues[0][0] != ' ') {
             if (fieldValues[0][0] == 'X') {
                 return "X wins.";
             } else {
                 return "O wins.";
             }
         }
-        if (fieldValues[0][2] == fieldValues[1][1] && fieldValues[0][2] == fieldValues[2][0]) {
+        if (fieldValues[0][2] == fieldValues[1][1] && fieldValues[1][1] == fieldValues[2][0] && fieldValues[0][2] != ' ') {
             if (fieldValues[0][2] == 'X') {
                 return "X wins.";
             } else {
@@ -87,7 +112,9 @@ public class Main {
     }
 
     private static void draw(char[][] fieldValues) {
+        System.out.println("\t---------");
         for (int i = 0; i < 3; i++) {
+            System.out.print("\t| ");
             for (int j = 0; j < 3; j++) {
                 if (j < 2) {
                     System.out.print(fieldValues[i][j] + " ");
@@ -95,8 +122,9 @@ public class Main {
                     System.out.print(fieldValues[i][j]);
                 }
             }
+            System.out.print(" |");
             System.out.println();
         }
+        System.out.println("\t---------");
     }
-
 }
