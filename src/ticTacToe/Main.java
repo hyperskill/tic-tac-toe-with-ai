8509d17be2db8dc710427a7b5acc24b41324408b
SashaRoyenko <sasharoyenko@gmail.com>
2019-01-27 20:22:01 +0200

    Task 2

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,9 +1,17 @@
-//package ticTacToe;
+package ticTacToe;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X" + " "+ "O" + " " + "X");
-        System.out.println("O" + " "+ "X" + " " + "o");
-        System.out.println("X" + " "+ "O" + " " + "X");
+        Scanner scanner = new Scanner(System.in);
+        int N = 3;
+        char[][] field = {{'O',' ', 'O'},{'X','X','O'},{' ', 'X', 'X'}};
+        for(int i =0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
