2c7d497dcb2c0f407d3f5f257782a56f0487c5bc
SashaRoyenko <sasharoyenko@gmail.com>
2019-02-01 17:02:35 +0200

    Stage 5. Added check for human moves.

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,21 +2,15 @@ package ticTacToe;
 
 import java.util.Scanner;
 public class Main {
-    /**
-     * Variables
-     */
+    /* ---Variables--- */
     public static Scanner scanner = new Scanner(System.in);
     public static int fieldSize = 3;
     public static char[][] field = new char[fieldSize][fieldSize];
     public static char computer = 'O';
     public static char human = 'X';
-    /**
-     * Functions
-     */
-
-    /**
-     * Check
-    */
+    /*-----*/
+    /* ----Functions--- */
+    /* Check for winnings*/
     public static boolean checkDiagonal(char symb){
         boolean flag = true;
         for(int i = 0; i < fieldSize; i++){
@@ -75,19 +69,45 @@ public class Main {
         }
         System.out.println();
     }
-    /**
-     * Move
-     */
+    /*-----*/
+    /* Check field */
+    public static boolean checkField(int x, int y){
+        if(x < 0 || x >= 3 || y < 0 || y >= 3) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+        else if(field[x][y] != ' '){
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+        return true;
+    }
+    /*-----*/
+    /* Move */
     public static void humanMove(){
-        System.out.print("Enter the coordinates: ");
-        int x = scanner.nextInt() - 1;
-        int y = scanner.nextInt() - 1;
+        int x;
+        int y;
+        do {
+            System.out.print("Enter the coordinates: ");
+            if(!scanner.hasNextInt() || !scanner.hasNextInt()) {
+                scanner.next();
+                scanner.next();
+                System.out.println("You should enter numbers!");
+            }
+            else {
 
-        field[x][y ] = human;
-}
-    /**
-     *Show
-     */
+                x = scanner.nextInt() - 1;
+                y = scanner.nextInt() - 1;
+                if(checkField(x, y)){
+                    field[x][y] = human;
+                    break;
+                }
+            }
+
+        }while(true);
+    }
+    /*-----*/
+    /* Show */
     public static void showField(){
         System.out.println("---------");
         for(int i =0; i < fieldSize; i++){
@@ -99,52 +119,17 @@ public class Main {
         }
         System.out.println("---------");
     }
-
-    /**
-     * Main
-     */
+    /*----------*/
     public static void main(String[] args) {
         char[][] arr = {
-                {'X', ' ', 'X'},
-                {' ', 'O', ' '},
-                {' ', ' ', ' '}
+            {' ', 'X', 'X'},
+            {'O', 'O', ' '},
+            {'O', 'X', ' '}
         };
         field = arr;
         showField();
         humanMove();
         showField();
         //isWin();
-       /* char[][] arr1 = {
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
-        field = arr1;
-        showField();
-        isWin();
-        char[][] arr2 = {
-                {'X', 'O', 'O'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
-        field = arr2;
-        showField();
-        isWin();
-        char[][] arr3 = {
-                {'X', 'O', 'X'},
-                {'O', 'O', 'X'},
-                {'X', 'X', 'O'}
-        };
-        field = arr3;
-        showField();
-        isWin();
-        char[][] arr4 = {
-                {'X', 'O', ' '},
-                {'O', 'O', 'X'},
-                {' ', 'X', ' '}
-        };
-        field = arr4;
-        showField();
-        isWin();*/
    }
 }
