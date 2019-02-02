adc13ef014296cb7280e6c29f564f2e33b4da1bb
Drelagreen <dmitry.zhalnin@europe.com>
2019-02-02 23:21:25 +0300

    Created + EX1

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,6 +2,13 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        char[][] field = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = i==0 ? 'X' : 'O';
+                System.out.print(field[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
