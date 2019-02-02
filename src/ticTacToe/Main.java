0587da3158f7088196cb3c2ef44e49f8750c4a06
Drelagreen <dmitry.zhalnin@europe.com>
2019-02-02 23:50:12 +0300

    STAGE 3

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,14 +1,37 @@
 package ticTacToe;
 
 public class Main {
+    private static char[][] field;
+
     public static void main(String[] args) {
-        char[][] field = new char[3][3];
+
+        field = new char[][]{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        print();
+
+    }
+
+    static void print() {
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                field[i][j] = i==0 ? 'X' : 'O';
-                System.out.print(field[i][j]+" ");
+                System.out.print(field[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println(gameStatus());
+    }
+
+    static String gameStatus() {
+        for (int i = 0; i < 3; i++) {
+
+            if (field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][0] != ' ') {
+                return field[i][0] == 'X' ? "X WINS!" : "O WINS!";
+            } else if (field[0][i] == field[1][i] && field[1][i] == field[2][i] && field[0][i] != ' ') {
+                return field[0][i] == 'X' ? "X WINS!" : "O WINS!";
+            }
+        }
+        if (field[1][1] == field[0][0] && field[1][1] == field[2][2] || field[1][1] == field[0][2] && field[1][1] == field[2][0]) {
+            return field[1][1] == 'X' ? "X WINS!" : field[1][1] == 'O' ? "O WINS!" : "GAME IS RUNNING";
+        } else return "GAME IS RUNNING";
     }
 }
