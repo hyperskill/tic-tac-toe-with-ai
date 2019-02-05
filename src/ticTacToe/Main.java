0d9454b9e3d2d850a6fba7e2413c681900a92a0f
SashaRoyenko <sasharoyenko@gmail.com>
2019-02-05 15:33:27 +0200

    Stage 9. Created a "medium" difficulty level

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -7,12 +7,7 @@ public class Main {
     /* ---Variables--- */
     public static Scanner scanner = new Scanner(System.in);
     public static int fieldSize = 3;
-    public static char[][] field = {
-                                    {' ', ' ', ' '},
-                                    {' ', ' ', ' '},
-                                    {' ', ' ', ' '} };
-    public static char computer = 'O';
-    public static char human = 'X';
+    public static char[][] field = new char[fieldSize][fieldSize];
     /*-----*/
     public static void main(String[] args) {
        startMenu();
@@ -107,8 +102,8 @@ public class Main {
     /*-----*/
     /* Check */
     public static boolean checkField(int x, int y){
-        if(x < 0 || x >= 3 || y < 0 || y >= 3) {
-            System.out.println("Coordinates should be from 1 to 3!");
+        if(x < 0 || x >= fieldSize || y < 0 || y >= fieldSize) {
+            System.out.println("Coordinates should be from 1 to " + fieldSize + "!");
             return false;
         }
         else if(field[x][y] != ' '){
@@ -136,46 +131,88 @@ public class Main {
     public static void startGame(String firstMode, String secondMode){
         clearField();
         if (firstMode.equals("user") && secondMode.equals("user")){
-            huamanVsUser();
+            humanVsHuman();
         }
         else if (firstMode.equals("user")){
-            huamanVsSBot(secondMode);
+            humanVsSBot(secondMode);
         }
         else if (secondMode.equals("user")){
-            botVsHuaman(firstMode);
+            botVsHuman(firstMode);
         }
         else {
             botVsBot(firstMode, secondMode);
         }
     }
     /*Game modes*/
-    public static void huamanVsUser(){
+    public static void humanVsHuman(){
         do{
             humanMove('X');
-            if(isWin()){ break;}
+            if(isWin()){ break; }
             humanMove('O');
         }while(!isWin());
     }
-    public static void huamanVsSBot(String mode){
-        do{
-            humanMove('X');
-            if(isWin()){ break;}
-            easyLevel('O');
-        }while(!isWin());
+    public static void humanVsSBot(String mode){
+        switch(mode){
+            case "easy":
+                do{
+                    humanMove('X');
+                    if(isWin()){ break;}
+                    easyLevel('O');
+                }while(!isWin());
+                break;
+            case "medium":
+                do{
+                    humanMove('X');
+                    if(isWin()){ break;}
+                    mediumLevel('O');
+                }while(!isWin());
+                break;
+        }
+
     }
-    public static void botVsHuaman(String mode){
-        do{
-            easyLevel('X');
-            if(isWin()){ break;}
-            humanMove('O');
-        }while(!isWin());
+    public static void botVsHuman(String mode){
+        switch(mode){
+            case "easy":
+                do{
+                    easyLevel('X');
+                    if(isWin()){ break;}
+                    humanMove('O');
+                }while(!isWin());
+                break;
+            case "medium":
+                do{
+                    mediumLevel('O');
+                    if(isWin()){ break;}
+                    humanMove('X');
+                }while(!isWin());
+                break;
+        }
+
     }
     public static void botVsBot(String firstMode, String secondMode){
-        do{
-            easyLevel('X');
+        do {
+            switch (firstMode){
+                case "easy":
+                    easyLevel('X');
+                    break;
+                case "medium":
+                    mediumLevel('X');
+                    break;
+                case "hard":
+                    break;
+            }
             if(isWin()){ break;}
-            easyLevel('O');
-        }while(!isWin());
+             switch (secondMode){
+                case "easy":
+                    easyLevel('O');
+                    break;
+                case "medium":
+                    mediumLevel('O');
+                    break;
+                case "hard":
+                break;
+                }
+        }while (!isWin());
     }
     /* ----- */
     /* Move */
@@ -221,14 +258,192 @@ public class Main {
         }
         showField();
     }
+    public static void mediumLevel(char symb){
+        showField();
+        System.out.println("Making move level \"medium\"");
+        Random random = new Random();
+        int indexI;
+        int indexJ;
+        char rival;
+        switch (symb){
+            case 'X':
+                rival = 'O';
+                break;
+            case 'O':
+                rival = 'X';
+                break;
+            default:
+                return;
+        }
+
+        while(true){
+            // Check winning
+            for (int i = 0; i < fieldSize; i++) {
+                for (int j = 0; j < fieldSize - 2; j++) {
+                    // Check winning vertical rectangle
+                    // Check 'X' 'X' ' '
+                    if (field[i][j] == symb && field[i][j + 1] == symb && field[i][j + 2] == ' ') {
+                        field[i][j + 2] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check ' ' 'X' 'X'
+                    if (field[i][j + 1] == symb && field[i][j + 2] == symb && field[i][j] == ' ') {
+                        field[i][j] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check 'X' ' ' 'X'
+                    if (field[i][j] == symb && field[i][j + 2] == symb && field[i][j + 1] == ' ') {
+                        field[i][j + 1] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check winning horizontal rectangle
+                    // Check 'X' 'X' 'O'
+                    if (field[j][i] == symb && field[j + 1][i] == symb && field[j + 2][i] == ' ') {
+                        field[j + 2][i] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check ' ' 'X' 'X'
+                    if (field[j + 1][i] == symb && field[j + 2][i] == symb && field[j][i] == ' ') {
+                        field[j][i] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check 'X' ' ' 'X'
+                    if (field[j][i] == symb && field[j+ 2][i] == symb && field[j  + 1][i] == ' ') {
+                        field[j  + 1][i] = symb;
+                        showField();
+                        return;
+                    }
+
+                }
+            }
+            // Check winning diagonal
+            for(int i = 0; i < fieldSize - 2; i++){
+                for(int j = 0; j < fieldSize -2; j++) {
+                    if (field[i][j] == symb && field[i + 1][j + 1] == symb && field[i + 2][j + 2] == ' ') {
+                        field[i + 2][j + 2] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i + 1][j + 1] == symb && field[i + 2][j + 2] == symb && field[i][j] == ' ') {
+                        field[i][j] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i][j] == symb && field[i + 2][j + 2] == symb && field[i + 1][j + 1] == ' ') {
+                        field[i + 1][j + 1] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i][j + fieldSize - 1] == symb && field[i + 1][j + 1] == symb && field[i + fieldSize - 1][j] == ' ') {
+                        field[i + fieldSize - 1][j] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i  + fieldSize - 1][j] == symb && field[i + 1][j + 1] == symb && field[i][j + fieldSize - 1] == ' ') {
+                        field[i][j + fieldSize - 1] = symb;
+                        showField();
+                        return;
+                    }
+                }
+            }
+            // Check block
+            for (int i = 0; i < fieldSize; i++) {
+                for (int j = 0; j < fieldSize - 2; j++) {
+                    // Check block horizontal rectangle
+                    // Check 'O' 'O' ' '
+                    if (field[i][j] == rival && field[i][j + 1] == rival && field[i][j + 2] == ' ') {
+                        field[i][j + 2] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check 'O' ' ' 'O'
+                    if (field[i][j] == rival && field[i][j + 2] == rival && field[i][j + 1] == ' ') {
+                        field[i][j + 1] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check ' ' 'O' 'O'
+                    if (field[i][j + 1] == rival && field[i][j + 2] == rival && field[i][j] == ' ') {
+                        field[i][j] = symb;
+                        showField();
+                        return;
+                    }
+                    //Check block vertical rectangle
+                    // Check 'O' 'O' ' '
+                    if (field[j][i] == rival && field[j  + 1][i] == rival && field[j + 2][i] == ' ') {
+                        field[j + 2][i] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check 'O' ' ' 'O'
+                    if (field[j][i] == rival && field[j+ 2][i] == rival && field[j+ 2][i] == ' ') {
+                        field[j + 1][i] = symb;
+                        showField();
+                        return;
+                    }
+                    // Check ' ' 'O' 'O'
+                    if (field[j + 1][i] == rival && field[j + 2][i] == rival && field[j][i] == ' ') {
+                        field[j][i] = symb;
+                        showField();
+                        return;
+                    }
+                }
+            }
+            // Check block diagonal
+            for(int i = 0; i < fieldSize - 2; i++){
+                for(int j = 0; j < fieldSize -2; j++) {
+                    if (field[i][j] == rival && field[i + 1][j + 1] == rival && field[i + 2][j + 2] == ' ') {
+                        field[i + 2][j + 2] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i + 1][j + 1] == rival && field[i + 2][j + 2] == rival && field[i][j] == ' ') {
+                        field[i][j] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i][j] == rival & field[i + 2][j + 2] == rival && field[i + 1][j + 1] == ' ') {
+                        field[i + 1][j + 1] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i][j + fieldSize - 1] == rival && field[i + 1][j + 1] == rival && field[i + fieldSize - 1][j] == ' ') {
+                        field[i + fieldSize - 1][j] = symb;
+                        showField();
+                        return;
+                    }
+                    if (field[i  + fieldSize - 1][j] == rival && field[i + 1][j + 1] == rival && field[i][j + fieldSize - 1] == ' ') {
+                        field[i][j + fieldSize - 1] = symb;
+                        showField();
+                        return;
+                    }
+                }
+            }
+            while(true){
+                indexI = random.nextInt(3);
+                indexJ = random.nextInt(3);
+                if(field[indexI][indexJ] == ' '){
+                    field[indexI][indexJ] = symb;
+                    showField();
+                    return;
+                }
+            }
+
+        }
+    }
     /* ----- */
     /*Clear Field*/
     public static void clearField(){
-        char[][] temp = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '} };
-        field = temp;
+        for(int i = 0; i < fieldSize; i++){
+            for(int j = 0; j < fieldSize; j++) {
+                field[i][j] = ' ';
+            }
+        }
     }
     /* Show */
     public static void showField(){
