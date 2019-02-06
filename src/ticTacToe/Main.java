7db1f4acd0997d074903408e65fd966dfe7d5e07
getPL <47267794+getPL@users.noreply.github.com>
2019-02-06 16:17:07 +1000

    stage #2
    Prints random-generated tic-tac-toe field.

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,9 +1,30 @@
 package ticTacToe;
 
-class Main {
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[][] field = new char[3][3];
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                switch (random.nextInt(3)) {
+                    case 0:
+                        field[i][j] = ' ';
+                        break;
+                    case 1:
+                        field[i][j] = 'X';
+                        break;
+                    case 2:
+                        field[i][j] = 'O';
+                        break;
+                }
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
