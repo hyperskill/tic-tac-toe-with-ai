e3483521eb92f43c87cc0fa07871e27af22cc01c
Daulet Syrymbetov <mr.grandapple1991@gmail.com>
2019-02-06 18:46:58 +0600

    simple game field

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,8 +2,10 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[] arr = new char[] {'O', ' ', 'O', 'X', 'X', 'O', ' ', 'X', 'X'};
+
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + '\n' +
+                          arr[3] + " " + arr[4] + " " + arr[5] + '\n' +
+                          arr[6] + " " + arr[7] + " " + arr[8]);
     }
 }
