0d7ee230f03173591559238bf756caa154766eed
getPL <47267794+getPL@users.noreply.github.com>
2019-02-12 22:18:51 +1000

    Update Main.java

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -1,9 +1,51 @@
-package ticTacToe;
-
 import java.util.Arrays;
 import java.util.Random;
 
-public class Main {
+public class project {
+
+    private static String result(char[][] field) {
+        int i, j;
+        boolean xWon = false;
+        boolean oWon = false;
+        boolean notFinished = false;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (field[i][j] == ' ') {
+                    notFinished = true;
+                }
+            }
+            if (field[i][0] == field[i][1] && field[i][0] == field[i][2]) {
+                if (field[i][0] == 'O') {
+                    oWon = true;
+                } else if (field[i][0] == 'X') {
+                    xWon = true;
+                }
+            }
+            if (field[0][i] == field[1][i] && field[0][i] == field[2][i]) {
+                if (field[0][i] == 'O') {
+                    oWon = true;
+                } else if (field[0][i] == 'X') {
+                    xWon = true;
+                }
+            }
+        }
+
+
+        if (xWon && oWon) {
+            return "It is not real.";
+        } else if (xWon) {
+            return "X wins";
+        } else if (oWon) {
+            return "O wins";
+        } else if (notFinished) {
+            return "Game not finished";
+        } else {
+            return "Draw";
+        }
+
+    }
+
     public static void main(String[] args) {
         char[][] field = new char[3][3];
         Random random = new Random();
@@ -25,6 +67,6 @@ public class Main {
             }
             System.out.println();
         }
-
+        System.out.println(result(field));
     }
 }
