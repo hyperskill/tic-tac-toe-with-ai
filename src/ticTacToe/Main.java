4d7bdc4d1aca2bf3105f0951c7ac2bc5c4a9718e
Daulet Syrymbetov <mr.grandapple1991@gmail.com>
2019-02-19 12:35:56 +0600

    print the game state

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -2,10 +2,74 @@ package ticTacToe;
 
 public class Main {
     public static void main(String[] args) {
-        char[] arr = new char[] {'O', ' ', 'O', 'X', 'X', 'O', ' ', 'X', 'X'};
+        char[][] arr1 = {
+            {'X', 'X', 'X'},
+            {'O', 'O', ' '},
+            {' ', 'O', ' '}
+        };
 
-        System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + '\n' +
-                          arr[3] + " " + arr[4] + " " + arr[5] + '\n' +
-                          arr[6] + " " + arr[7] + " " + arr[8]);
+        char[][] arr2 = {
+            {'X', 'O', 'X'},
+            {'O', 'X', 'O'},
+            {'X', 'X', 'O'}
+        };
+
+        char[][] arr3 = {
+            {'X', 'O', 'O'},
+            {'O', 'X', 'O'},
+            {'X', 'X', 'O'}
+        };
+
+        char[][] arr4 = {
+            {'X', 'O', 'X'},
+            {'O', 'O', 'X'},
+            {'X', 'X', 'O'}
+        };
+
+        char[][] arr5 = {
+            {'X', 'O', ' '},
+            {'O', 'O', 'X'},
+            {' ', 'X', ' '}
+        };
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("X wins" + '\n');
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("X wins" + '\n');
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("O wins" + '\n');
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr4[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Draw" + '\n');
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr5[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Game not finished" + '\n');
     }
 }
