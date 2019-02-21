f9bb0320da215c0ce7f3af622d159ef8f1bd36cc
mrkam <mr.kamc@gmail.com>
2019-02-21 09:58:57 +0300

    edited output

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -6,6 +6,7 @@ public class Main {
         ticTac = new char[]{'0', ' ', '0', 'X', 'X', ' ', '0', 'X', '0'};
         for(int i = 0; i < ticTac.length; i+=3) {
             System.out.println(ticTac[i] + " " + ticTac[i+1] + " " + ticTac[i+2]);
+            System.out.println();
         }
     }
 }
